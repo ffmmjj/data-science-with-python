af093c59fe7d11ab71455a942f6518e187df78c2
João Paulo Leonidas Fernandes Dias da Silva <jsilva@thoughtworks.com>
2016-08-27 19:03:27 -0300

    Train polynomial and RBF models.

--- prediction-classification/svm.py
+++ prediction-classification/svm.py
@@ -7,7 +7,7 @@ SVM Classification
 =========================================================
 
 The first plot displays the iris classification training data.
-The second one shows the predictions given a new dataset.
+The second one shows the predictionLinears given a new dataset.
 
 If you don't see anything, try Alt+Tab or Cmd+Tab to switch to the plot window.
 
@@ -38,15 +38,34 @@ def plotIrisData(figure, X, y):
 	plt.xticks(())
 	plt.yticks(())
 
-def plotDecisionBoundary(figure, X, Y, Z):
+def plotDecisionBoundary(figure, X, Y, Z_Linear):
 	# Create color maps
 	cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 	cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 	# Put the result into a color plot
-	Z = Z.reshape(xx.shape)
+	Z_Linear = Z_Linear.reshape(xx.shape)
 
-	plt.pcolormesh(X, Y, Z, cmap=cmap_light)
+	plt.pcolormesh(X, Y, Z_Linear, cmap=cmap_light)
+
+def plotModel(X, Y, Z, testData, testTarget, newData, prediction):
+		# Create first figure.
+	figure = plt.figure()
+
+	# Plot decision boundary.
+	plotDecisionBoundary(figure, X, Y, Z)
+
+	# Plot training points.
+	plotIrisData(figure, testData, testTarget)
+
+	# Create second figure.
+	figure = plt.figure()
+
+	# Plot decision boundary.
+	plotDecisionBoundary(figure, X, Y, Z)
+
+	# Plot predicted points.
+	plotIrisData(figure, newData, prediction)
 
 # Load the dataset.
 iris = datasets.load_iris()
@@ -58,38 +77,37 @@ Y = iris.target
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 
-svc = svm.SVC(kernel='linear')
+# Train a linear model.
+svcLinear = svm.SVC(kernel='linear')
+svcLinear.fit(X, Y) 
 
-# Train the model.
-svc.fit(X, Y) 
+# Train a polynomial model.
+svcPoly = svm.SVC(kernel='poly')
+svcPoly.fit(X, Y) 
+
+# Train a RBF model.
+svcRBF = svm.SVC(kernel='rbf')
+svcRBF.fit(X, Y) 
 
 # Create new data points.
 newData = np.array([[4, 2],[6,3],[7,3.5],[3,4],[5,2.5],[5,4],[5,2]])
-prediction = svc.predict(newData)
+predictionLinear = svcLinear.predict(newData)
+predictionPoly = svcPoly.predict(newData)
+predictionRBF = svcRBF.predict(newData)
 
 h = .02
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 
 # Predict values for descision boundary.
-Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z_Linear = svcLinear.predict(np.c_[xx.ravel(), yy.ravel()])
+Z_Poly = svcPoly.predict(np.c_[xx.ravel(), yy.ravel()])
+Z_RBF = svcRBF.predict(np.c_[xx.ravel(), yy.ravel()])
 
-# Create first figure.
-figure = plt.figure()
+plotModel(xx, yy, Z_Linear, X, Y, newData, predictionLinear)
 
-# Plot decision boundary.
-plotDecisionBoundary(figure, xx, yy, Z)
+plotModel(xx, yy, Z_Poly, X, Y, newData, predictionPoly)
 
-# Plot training points.
-plotIrisData(figure, X, Y)
-
-# Create second figure.
-figure = plt.figure()
-
-# Plot decision boundary.
-plotDecisionBoundary(figure, xx, yy, Z)
-
-# Plot predicted points.
-plotIrisData(figure, newData, prediction)
+plotModel(xx, yy, Z_RBF, X, Y, newData, predictionRBF)
 
 plt.show()
